f70b1ef1594f4cd7b833c5b40ba016d949e2eac0
noamshmuely <noam.shmuely.08@gmail.com>
2026-09-08 11:18:35 +0300

    Add files via upload

---
+++ soldier.py
@@ -0,0 +1,58 @@
+import consts
+
+soldier = {}
+
+def create_soldier():
+    global soldier
+    soldier = {"row": 0, "col": 0}
+
+def can_move(direction):
+    if direction == consts.UP:
+        if soldier["row"] > 0:
+            return True
+    if direction == consts.DOWN:
+        if soldier["row"]+3 < consts.BOARD_ROWS:
+            return True
+    if direction == consts.LEFT:
+        if soldier["col"] > 0:
+            return True
+    if direction == consts.RIGHT:
+        if soldier["col"] + 1 < consts.BOARD_COLS:
+            return True
+
+
+
+def move(direction):
+    global soldier
+    if direction == consts.UP and can_move(direction):
+        soldier["row"] += 1
+
+    elif direction == consts.DOWN and can_move(direction):
+        soldier["row"] -= 1
+
+    elif direction == consts.LEFT and can_move(direction):
+        soldier["col"] -= 1
+
+    elif direction == consts.RIGHT and can_move(direction):
+        soldier["col"] += 1
+
+
+def get_soldier_body():
+    global soldier
+    soldier_body = [(soldier["row"], soldier["col"]),
+                    (soldier["row"], soldier["col"]+1),
+                    (soldier["row"]+1, soldier["col"]),
+                    (soldier["row"]+1, soldier["col"]+1),
+                    (soldier["row"]+2, soldier["col"]+1),
+                    (soldier["row"]+2, soldier["col"])]
+    return soldier_body
+
+
+def get_soldier_feet():
+    global soldier
+    soldier_feet = [(soldier["row"]+3, soldier["col"]),
+                    (soldier["row"]+3, soldier["col"]+1)]
+    return soldier_feet
+
+
+
