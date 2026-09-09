666ff3a73bfe8866091dbf59cd198f422d661e86
Tal <talwaserman1@gmail.com>
2026-09-09 11:55:07 +0300

    db

--- database.py
+++ database.py
@@ -1,32 +1,56 @@
 import pandas as pd
 import consts
+import json
+
 
 def save(grid, index):
     df = pd.DataFrame(grid)
-    df.to_json("data.json", orient="split", compression="infer")
+    db = load_db()
+    jdb = df.to_json()
+    db[index] = jdb
+    save_db(db)
+
+
 
 def load(index):
-    df = pd.read_json("data.json", orient="split", compression="infer")
-    grid = df.values.tolist()
+    db = load_db()
+    df = json.loads(db[index])
+    d = pd.DataFrame(df)
+    grid = d.values.tolist()
     return grid
+
 
 grid = [[1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]]
-# save(grid)
-# print("=======")
-# grid = load()
-# print(grid)
+
+
 
 
 def start_db():
     grid = []
     df = pd.DataFrame(grid)
-    with open("data.json", "w") as f:
-        for _ in range(9):
-            df.to_csv(f, index=False, mode='a', lineterminator='\n')
+    with open("data.json", "a") as f:
+        pass
+    with open("data.json", "r") as f:
+        lines = f.readlines()
+    if len(lines) == 0:
+        with open("data.json", "a") as f:
+            for _ in range(9):
+                jdf = df.to_json()
+                f.write(jdf + " ")
 
-# start_db()
-save(grid, 0)
-grid = load()
-print(grid)
+
+def load_db():
+    with open("data.json", "r") as f:
+        lines = f.readlines()
+        return lines[0].split(" ")
+
+
+def save_db(lines):
+    with open("data.json", "w") as f:
+        for line in lines:
+            f.write(line + " ")
+
+
+
