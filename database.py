7a1f22fa0ae4bbf26f97579ff9c9174ca74f05cc
Tal <talwaserman1@gmail.com>
2026-09-09 10:56:53 +0300

    f

--- database.py
+++ database.py
@@ -1,18 +1,32 @@
 import pandas as pd
+import consts
 
-
-def save(grid):
+def save(grid, index):
     df = pd.DataFrame(grid)
-    print(df)
-    df.to_csv("data.csv", lineterminator='\n', header=False)
+    df.to_json("data.json", orient="split", compression="infer")
 
-def load():
-    with open("data.csv", "r") as file:
-        df = pd.read_csv(file)
-        print(df)
-        l = df.values.tolist()
-        print(l)
+def load(index):
+    df = pd.read_json("data.json", orient="split", compression="infer")
+    grid = df.values.tolist()
+    return grid
 
-grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-save(grid)
-load()
+grid = [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]]
+# save(grid)
+# print("=======")
+# grid = load()
+# print(grid)
+
+
+def start_db():
+    grid = []
+    df = pd.DataFrame(grid)
+    with open("data.json", "w") as f:
+        for _ in range(9):
+            df.to_csv(f, index=False, mode='a', lineterminator='\n')
+
+# start_db()
+save(grid, 0)
+grid = load()
+print(grid)
