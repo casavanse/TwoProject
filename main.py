d4c3e4e4e20e7174178253ae2f470975f91ae6e6
Tal <talwaserman1@gmail.com>
2026-09-09 11:56:52 +0300

    f

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def handle_user_events():
         if event.type == pygame.KEYUP:
             if event.key in consts.KEYS_DICT.keys():
                 end = time.time() 
-                 duration = start - end
+                duration = start - end
                 if duration <= consts.ONE_SECOND:
                         save(consts.KEYS_DICT[event.key])
                 elif duration > consts.ONE_SECOND:
