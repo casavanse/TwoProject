b5619f4fbebb1d9ac7b1576867daf4cafdbb35eb
Tal <talwaserman1@gmail.com>
2026-09-09 10:08:41 +0300

    f

--- database.py
+++ database.py
@@ -1,12 +1,18 @@
 import pandas as pd
 
-def save(grid):
-    with open("data.csv", "a") as file:
-        df = pd.DataFrame(grid)
-        print(df)
-        df.to_csv(file)
 
-grid = [[1,2,3],
-        [4,5,6],
-        [7,8,9]]
+def save(grid):
+    df = pd.DataFrame(grid)
+    print(df)
+    df.to_csv("data.csv", lineterminator='\n', header=False)
+
+def load():
+    with open("data.csv", "r") as file:
+        df = pd.read_csv(file)
+        print(df)
+        l = df.values.tolist()
+        print(l)
+
+grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 save(grid)
+load()
